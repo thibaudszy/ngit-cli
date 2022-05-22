64c579a5bfa3ec5d6b824873609b05de1bbd6c5d
Thibaud Szymczak <thibaudszy@gmail.com>
2022-05-22 17:23:54 +0200

    Cleanup

--- src/scripts/select-branch.js
+++ src/scripts/select-branch.js
@@ -1,12 +1,14 @@
 import clipboard from 'clipboardy';
 import { getBranches, branchDataAsObjects } from '../utils/utils.js';
-import { branchSelect } from '../utils/promptBranchSelect.js';
+import { branchMultiSelect } from '../utils/promptBranchSelect.js';
+import chalk from 'chalk';
 
 export default async function () {
     const refs = await getBranches({ sort: '-committerdate' });
     const options = branchDataAsObjects(refs);
-    const selection = await branchSelect(options);
+    const selection = await branchMultiSelect(options);
 
-    console.log('Copied to clipboard:', selection);
-    clipboard.writeSync(selection);
+    const inlinedSelection = selection.join(' ');
+    console.log(chalk.bold(chalk.blueBright('Copied to clipboard:')), inlinedSelection);
+    clipboard.writeSync(inlinedSelection);
 }
