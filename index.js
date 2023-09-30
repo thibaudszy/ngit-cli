d6040ee8f048326f33774ffa9d233ef037243658
Thibaud Szymczak <thibaudszy@gmail.com>
2023-09-30 22:44:57 +0200

    allow to exit by pressing q

--- index.js
+++ index.js
@@ -3,6 +3,27 @@
 import chalk from 'chalk';
 import getAndRemoveFlag from './src/utils/getAndRemoveFlag.js';
 
+// Exit program on `q` key press
+// --------------------------------------
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (data) => {
+    const input = data.toString().trim(); // Convert buffer to string and remove whitespace.
+    if (input === 'q') {
+        process.exit();
+    }
+});
+
+// Put the standard input into "raw" mode, so keys are read as they are pressed.
+process.stdin.setRawMode(true);
+
+// Resume standard input to start receiving "data" events.
+process.stdin.resume();
+
+// --------------------------------------
+// --------------------------------------
+
 let [, , command, ...args] = process.argv;
 
 const script = await import(`./src/scripts/${command}.js`);
