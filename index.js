c5070a557fe009b277e2c3abff6862081b8493f9
thibaud.szymczak <thibaud.szymczak@datadoghq.com>
2025-12-14 10:23:18 +0100

    show interactive command selector when no command is provided

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable -- eslint can't parse top level imports yet */
 import chalk from 'chalk';
+import inquirer from 'inquirer';
 import getAndRemoveFlag from './src/utils/getAndRemoveFlag.js';
 
 // Exit program on `q` key press
@@ -24,9 +25,36 @@ process.stdin.resume();
 // --------------------------------------
 // --------------------------------------
 
+const commands = [
+    { name: 'add', description: 'Stage files interactively' },
+    { name: 'checkout', description: 'Switch branches interactively' },
+    { name: 'erase', description: 'Delete branches interactively' },
+    { name: 'stash', description: 'Stash changes interactively' },
+    { name: 'select-branch', description: 'Copy branch names to clipboard' },
+    { name: 'select-status', description: 'Copy file names to clipboard' },
+];
+
 let [, , command, ...args] = process.argv;
 
+async function selectCommand() {
+    const { selected } = await inquirer.prompt({
+        type: 'list',
+        name: 'selected',
+        message: 'Select a command',
+        choices: commands.map(({ name, description }) => ({
+            name: `${name} - ${description}`,
+            value: name,
+        })),
+        pageSize: 10,
+    });
+    return selected;
+}
+
 try {
+    if (!command) {
+        command = await selectCommand();
+    }
+
     const script = await import(`./src/scripts/${command}.js`);
     console.clear();
     const dryRun = getAndRemoveFlag(args, '--ngit-dry-run').flag;
@@ -36,7 +64,7 @@ try {
 } catch (error) {
     if (error.code === 'ERR_MODULE_NOT_FOUND') {
         console.log(chalk.bgRed(`Unknown command: ${command}`));
-        console.log('Available commands: add, checkout, erase, stash, select-branch, select-status');
+        console.log('Available commands:', commands.map(c => c.name).join(', '));
     } else {
         console.log(chalk.bgRed('Exited with error'), error.stderr || error.message || error);
     }
